881c129e547d2b2eb32bfad5ed6d0e397d2970a2
angerangel@gmail.com <angerangel@gmail.com>
2013-03-25 11:32:38 +0100

    Added test file

--- builds/windows/t.r
+++
@@ -1,9 +0,0 @@
-rebol []
-
-
- view [
-  a: box 200x200 
-  button on-action [to-draw [pen white line 0x0 100x100] a/gob/draw
-   show a
-   ]
-]
---
+++ builds/windows/test.r
@@ -0,0 +1,7 @@
+rebol [
+title "test script"
+purpose: "put your test here, and launch t to test it"
+]
+
+
+print "test executed"
